e861593ee9b9989f7246546f5f0ad201dc600944
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-11-27 09:51:21 +0200

    implemented navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Nav from "./components/Nav";
 
-gsap.registerPlugin(useGSAP,ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Nav />
       <Hero />
       <About />
     </main>
